beae3af846b661b428c5552626bdfb881a30e19f
Catherine Tower <Calvin@CorianderDesigns.com>
2019-04-17 07:09:47 -0700

    Good news: it doesn't perform queued backups. I've added a usage function and a parse arguments stub for arguments that are not yet implemented, and added a debugging line so I can see what happens.

--- source/backupd.d
+++ source/backupd.d
@@ -3,15 +3,39 @@ import BackupJobQueue;
 import BackupFile;
 
 import core.thread;
+import std.stdio;
+
+enum string programName = "backupd";
 
 int main(string[] args) {
+
+  parseArguments(args);
 
   auto jobs = new BackupJobQueue("~/.backupconfig");
   while(1) {
     auto durations = jobs.getQueuedJobTimes();
     foreach(entry; durations) {
       Thread.sleep(entry.duration);
+
+      /* THIS IS FOR DEBUGGING */
+      writefln("Doing job %s", entry.index);
+      /* TAKE IT OUT LATER */
+
       jobs.jobs[entry.index].doBackup();
     }
   }
+}
+
+void parseArguments(string[] args) {
+
+}
+
+void usage(int status) {
+  writefln("Usage: %s [OPTIONS]\n", programName);
+  writeln("OPTIONS:");
+  writeln(" -n  --new      Create a new backup job");
+  writeln(" -l  --list     List existing backup jobs");
+
+  import core.stdc.stdlib : exit;
+  exit(status);
 }
